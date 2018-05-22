e19b4c9fa6f8e71074c1e4fb87cdb9a48f03a01a
A4213001 <A4213001@g.pccu.edu.tw>
2018-05-22 10:50:28 +0800

    20180522

---
+++ 814/814_1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root->left!=NULL)
+            root->left=pruneTree(root->left);
+        if(root->right!=NULL)
+            root->right=pruneTree(root->right);
+        if(root->left==NULL&&root->right==NULL&&root->val==0)
+            return NULL;
+        else
+            return root;
+    }
+};
+
+void preTraversal(TreeNode *root, vector<int> &res){
+    if (root == NULL)
+        return;
+    res.push_back(root->val);
+    preTraversal(root->left, res);
+    preTraversal(root->right, res);
+}
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    preTraversal(root, res);
+    return res;
+}
+
+int main(){
+	TreeNode *root=new TreeNode(1);
+	root->left=new TreeNode(1);
+	root->left->left=new TreeNode(1);
+	root->left->left->left=new TreeNode(0);
+	root->left->right=new TreeNode(1);
+	root->right=new TreeNode(0);
+	root->right->left=new TreeNode(0);
+	root->right->right=new TreeNode(1);
+	Solution().pruneTree(root);
+	for(auto i:preorderTraversal(root))
+		cout<<i<<" ";
+}
