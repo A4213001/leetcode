6d618a5f5a84878225cbfad9b9124413c9ef41f1
A4213001 <A4213001@g.pccu.edu.tw>
2018-05-23 20:47:07 +0800

    20180523

--- 806/806_1.cpp
+++ 806/806_1.cpp
@@ -1,17 +1,17 @@
+#include<vector>
+#include<string>
+using namespace std;
+
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string S) {
         vector<int> ans(2,0);
         int sum=0;
         ans[0]=1;
-        map<char,int> mapW;
-        for(char i='a';i<='z';i++){
-            mapW[i]=(widths[i-'a']);
-        }
         for(auto i:S){
-            sum+=mapW[i];
+            sum+=widths[i-'a'];
             if(sum>100){
-                sum=0+mapW[i];
+                sum=widths[i-'a'];
                 ans[0]++;
             }
         }
