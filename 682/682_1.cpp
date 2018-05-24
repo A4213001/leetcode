6dee6a47804031dfaf9e9d9ad1df5f617d54f45c
A4213001 <A4213001@g.pccu.edu.tw>
2018-05-25 01:34:35 +0800

    20180525

--- 682/682_1.cpp
+++ 682/682_1.cpp
@@ -4,40 +4,38 @@
 #include<string>
 using namespace std;
 
-int calPoints(vector<string>& ops) {
-    int sum=0;
-    string num;
-    stack<int> numStack;
-    for(int i=0;i<ops.size();i++){
-        if(ops[i][0]=='C')
-            numStack.pop();
-        else if(ops[i][0]=='D')
-            numStack.push(numStack.top()*2);
-        else if(ops[i][0]=='+'){
-            int top1=numStack.top();
-            numStack.pop();
-            int top2=numStack.top();
-            numStack.push(top1);
-            numStack.push(top1+top2);
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int sum=0;
+        stack<int> numStack;
+        for(auto i:ops){
+            if(i=="C")
+                numStack.pop();
+            else if(i=="D")
+                numStack.push(numStack.top()*2);
+            else if(i=="+"){
+                int top1=numStack.top();
+                numStack.pop();
+                int top2=numStack.top();
+                numStack.push(top1);
+                numStack.push(top1+top2);
+            }
+            else{
+                numStack.push(stoi(i));
+            }    
         }
-        else{
-			num="";
-        	for(int j=0;j<ops[i].size();j++){
-        		num+=ops[i][j];
-			}
-			numStack.push(stoi(num));
-		}    
+        while(!numStack.empty()){
+            sum+=numStack.top();
+            numStack.pop();
+        }
+        return sum;
     }
-    while(!numStack.empty()){
-        sum+=numStack.top();
-        numStack.pop();
-    }
-    return sum;
-}
+};
 
 int main(){
 	vector<string> ops={"5","-2","4","C","D","9","+","+"};
-	cout<<calPoints(ops);
+	cout<<Solution().calPoints(ops);
 }
 
 
